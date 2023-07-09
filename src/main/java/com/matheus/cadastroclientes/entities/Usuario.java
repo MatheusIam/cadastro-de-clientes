a6203d6c059e803642c044c3649e1b2b5ab1e7af
Matheus Iam <matheusiam27@gmail.com>
2023-07-09 03:53:30 -0400

    initial commit

---
+++ src/main/java/com/matheus/cadastroclientes/entities/Usuario.java
@@ -0,0 +1,76 @@
+package com.matheus.cadastroclientes.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	
+	private String senha;
+	
+	public Usuario () {}
+
+	public Usuario(Long id, String nome, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", senha=" + senha + "]";
+	}
+	
+}
---
+++ src/test/java/com/matheus/cadastroclientes/CadastroclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.cadastroclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
